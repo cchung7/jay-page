f9b78493c846f2e1f9bf0ab4073bef8be1a062c6
Jay Chung <chung_chul@yahoo.com>
2026-01-04 05:33:10 -0600

    JP_V1.0

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Mail, Rocket, Linkedin } from "lucide-react";
+import { ArrowRight, Mail, Rocket, MapPin, Linkedin } from "lucide-react";
 
 export const ContactSection = () => {
   return (
