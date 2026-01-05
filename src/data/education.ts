4e08c172c2ef10361f3c6cce2d5eb970d5a23dec
Jay Chung <chung_chul@yahoo.com>
2026-01-04 21:52:56 -0600

    JP_V1.0

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,12 +1,12 @@
 export const education = [
   { school: "University of Texas at Dallas", 
     degree: "B.S. Software Engineering", 
-    period: "Dec 2025", 
-    honors: "Relevant Courses: Artificial Intelligence, Database Systems, Software Architecture & Design, Software Testing & Verification, Requirements Engineering, Object-Oriented Design, Software Project Planning & Management, Operating Systems, Data Structures, Unix" 
+    period: "Graduated: Dec 2025", 
+    honors: "Relevant Courses: Artificial Intelligence, Requirements Engineering, Object-Oriented Design" 
   },
   { school: "University of Texas at Dallas", 
     degree: "B.S. Biochemistry", 
-    period: "Aug 2019", 
+    period: "Graduated: Aug 2019", 
     honors: "" 
   },
 ];
