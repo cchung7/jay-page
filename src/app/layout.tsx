64385889ac75ae0fe1180f65a5b2ac6c82ac605d
Jay Chung <chung_chul@yahoo.com>
2026-01-07 17:21:32 -0600

    JP_V1.0 error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
-import { CartProvider } from "@/lib/cart-context";
 import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,22 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} relative min-h-screen bg-background text-foreground scroll-smooth`}>
+      <body
+        className={`${inter.className} relative min-h-screen bg-background text-foreground scroll-smooth`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          <CartProvider>
-            {/* Added 'relative' position for scroll tracking */}
-            <div className="relative flex min-h-screen flex-col">
-              <Navbar />
-              <Toaster position="top-right" richColors />
-              <main className="flex-1">{children}</main>
-              <Footer />
-            </div>
-          </CartProvider>
+          <div className="relative flex min-h-screen flex-col">
+            <Navbar />
+            <Toaster position="top-right" richColors />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
